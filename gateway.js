1a0058206727752d1dd8d9345b438731b7b4d8af
AeroHyena <rakudreemurr@gmail.com>
2023-03-19 20:31:03 +0200

    Implemented homepage functionality into the gateway service. Added an api that deploys the gateway service, and imported it into the launch.js file.

--- gateway.js
+++ gateway.js
@@ -1,2 +1,26 @@
 // this is the api gateway used in this app.
 // it is responsible for reading requests and routing them to the appropriate services
+
+
+const express = require('express')
+const app = express()
+
+
+// Respond to a GET request on the main page
+app.get('/', (req, res) => {
+    console.log("rendering homepage");
+
+    res.send("Hello World");
+});
+
+
+
+// create the startGateway function that deploys the gateway service
+const startGateway = (port) => {
+    app.listen(port);
+    console.log("The gateway service is now deployed - listening on port " + port)
+}
+
+
+// export this module's api functions to be accessed by launch.js
+module.exports = { startGateway };
--- launch.js
+++ launch.js
@@ -1,1 +1,9 @@
 // responsible for initializing all the microservices and thier individual servers.
+
+// import all service apis
+const gateway = require("./gateway.js");
+
+
+
+// deploy the services
+gateway.startGateway(8000);
